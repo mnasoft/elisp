f5a919a2ce26ce47171958047bf9945a29654c5e
Nick Matvyeyev <mnasoft@gmail.com>
2019-03-28 22:58:29 +0200

    Чт мар 28 22:58:29 EET 2019

--- common-lisp.el
+++ common-lisp.el
@@ -1,28 +1,22 @@
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; SBCL -START ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;(cond
-;;  ((file-exists-p (concat (getenv "SBCL_HOME") "\\sbcl.exe"))
-;;   (setq inferior-lisp-program (concat (getenv "SBCL_HOME") "\\sbcl.exe")))
-;;  (t (message "Не могу найти файл %s. Переменная inferior-lisp-program не определена в файле %s"
-;;	      (concat (getenv "SBCL_HOME") "\\sbcl.exe")
-;;	      (buffer-name))))
-
-(setq inferior-lisp-program (concat (getenv "MSYS_HOME") "/usr/local/bin/sbcl.exe"))
+(cond
+ ((string= (system-name) "mnasoft-00")  (setq inferior-lisp-program "/usr/bin/sbcl"))
+ ((string= (system-name) "KO11-118383") (setq inferior-lisp-program (concat (getenv "MSYS_HOME") "/usr/local/bin/sbcl.exe"))))
 
 ;(setq inferior-lisp-program "D:\\PRG\\ccl\\wx86cl64.exe")
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; SBCL -STOP ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Slime.  Loading Contrib Packages - START ;;;;;;;;;;;;;
 ;;(setq slime-contribs '(slime-scratch slime-editing-commands))
 ;;(setq slime-contribs '(slime-repl))  ; repl only
 (setq slime-contribs '(slime-fancy)) ; almost everything
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; slime.  loading contrib packages - STOP ;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Slime.  Loading Contrib Packages - STOP ;;;;;;;;;;;;;;
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Slime autoload - Start 
+;; Slime autoload - Start ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (slime)
-;; Slime autoload - End
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Slime autoload - End ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+
+
