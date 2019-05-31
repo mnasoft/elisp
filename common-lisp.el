f0a3ada6791d30be5d5ce10db32df4e1e5f8c1be
Nick Matvyeyev <mnasoft@gmail.com>
2019-05-31 16:28:34 +0300

    31 май 2019 г. 16:28:33

--- common-lisp.el
+++ common-lisp.el
@@ -5,16 +5,25 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Slime.  Loading Contrib Packages - STOP ;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; SBCL -START ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(cond
- ((string= (system-name) "mnasoft-00")  (setq inferior-lisp-program "/usr/bin/sbcl"))
- ((string= (system-name) "KO11-118383") (setq inferior-lisp-program (concat (getenv "MSYS_HOME") "/usr/local/bin/sbcl.exe")))
- ((string= (system-name) "MNASOFT-10")  (setq inferior-lisp-program "D:/PRG/msys/usr/local/bin/sbcl.exe")))
+(setq inferior-lisp-program-name :sbcl)
+;;(setq inferior-lisp-program-name :allegro-express)
+
+(when (eq inferior-lisp-program-name :sbcl)
+  (cond
+   ((string= (upcase (system-name)) "MNASOFT-00")  (setq inferior-lisp-program "/usr/bin/sbcl"))
+   ((string= (upcase (system-name)) "KO11-118383") (setq inferior-lisp-program (concat (getenv "MSYS_HOME") "/usr/local/bin/sbcl.exe")))
+   ((string= (upcase (system-name)) "MNASOFT-10")  (setq inferior-lisp-program "D:/PRG/msys/usr/local/bin/sbcl.exe"))))
+
+(when (eq inferior-lisp-program-name :allegro-express)
+  (cond
+;;;; ((string= (system-name) "ko11-118383") (setq inferior-lisp-program "D:/PRG/acl10.1express/allegro-express.exe"))
+   ((string= (upcase (system-name)) "KO11-118383") (setq inferior-lisp-program "D:/PRG/acl10.1express/alisp.exe +B +m -e slime"))))
 
 ;(setq inferior-lisp-program "D:\\PRG\\ccl\\wx86cl64.exe")
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; SBCL -STOP ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Slime autoload - Start ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(slime)
+;;(slime)
 ;; Slime autoload - End ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
