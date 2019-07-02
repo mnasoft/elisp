8cb9f6237c9747afdab9c49fe5debe26c50cc0ae
Nick Matvyeyev <mnasoft@gmail.com>
2019-07-02 16:29:20 +0300

    2 июл 2019 г. 16:29:20

--- main.el
+++ main.el
@@ -35,3 +35,23 @@
 
 
 
+(hs-minor-mode)
+;;(add-hook 'prog-mode-hook #'hs-minor-mode)
+(global-set-key (kbd "C-c <right>") 'hs-show-block)
+(global-set-key (kbd "C-c <left>")  'hs-hide-block)
+
+(global-set-key (kbd "C-c <down>")  'hs-show-all)
+(global-set-key (kbd "C-c <up>")    'hs-hide-all)
+
+;;C-c @ C-a	hs-show-all
+;;C-c @ C-c	hs-toggle-hiding
+;;C-c @ C-d	hs-hide-block
+;;C-c @ C-e	hs-toggle-hiding
+;;C-c @ C-h	hs-hide-block
+;;C-c @ C-l	hs-hide-level
+;;C-c @ C-s	hs-show-block
+;;C-c @ C-t	hs-hide-all
+;;C-c @ ESC	Prefix Command
+
+;;C-c @ C-M-h	hs-hide-all
+;;C-c @ C-M-s	hs-show-all
