60c745756f8ae4d61b282372a7aa8e585fc3873d
Nick Matvyeyev <mnasoft@gmail.com>
2019-08-05 23:03:28 +0300

    Пн авг  5 23:03:28 EEST 2019

--- main.el
+++ main.el
@@ -26,6 +26,10 @@
 (load "~/elisp/zm-tz-header.el")
 (load "~/elisp/open-some-files.el")
 (load "~/elisp/translate-commands-gr-ru-uk.el")
+(load "~/elisp/hide-show-mode.el")
+(load "~/elisp/gnuplot.el")
+;;;; (load "~/elisp/slime_smartparens_auto-complete.el")
+
 
 ;; bigger latex fragment
 (setq org-format-latex-options (plist-put org-format-latex-options :scale 1.75))
@@ -34,24 +38,3 @@
 
 
 
-
-(hs-minor-mode)
-;;(add-hook 'prog-mode-hook #'hs-minor-mode)
-(global-set-key (kbd "C-c <right>") 'hs-show-block)
-(global-set-key (kbd "C-c <left>")  'hs-hide-block)
-
-(global-set-key (kbd "C-c <down>")  'hs-show-all)
-(global-set-key (kbd "C-c <up>")    'hs-hide-all)
-
-;;C-c @ C-a	hs-show-all
-;;C-c @ C-c	hs-toggle-hiding
-;;C-c @ C-d	hs-hide-block
-;;C-c @ C-e	hs-toggle-hiding
-;;C-c @ C-h	hs-hide-block
-;;C-c @ C-l	hs-hide-level
-;;C-c @ C-s	hs-show-block
-;;C-c @ C-t	hs-hide-all
-;;C-c @ ESC	Prefix Command
-
-;;C-c @ C-M-h	hs-hide-all
-;;C-c @ C-M-s	hs-show-all
--- org-babel-do-load-languages.el
+++ org-babel-do-load-languages.el
@@ -1,5 +1,14 @@
 ;;;; org-babel-do-load-languages.el
 
+(require 'package)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+
+(setq package-archive-priorities nil)
+(add-to-list 'package-archive-priorities '("org" . 5))
+(add-to-list 'package-archive-priorities '("melpa" . 4))
+(add-to-list 'package-archive-priorities '("built-in" . 3))
+          
+
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((ditaa   . t)
