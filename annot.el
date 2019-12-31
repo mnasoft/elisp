dff6cdfcbbcfa176e4aefd5d5e99ebfe78b2ca84
Nick Matvyeyev <mnasoft@gmail.com>
2019-12-31 18:49:41 +0200

    Вт дек 31 16:49:41 UTC 2019

---
+++ annot.el
@@ -0,0 +1,5 @@
+(add-to-list 'load-path "~/quicklisp/dists/quicklisp/software/cl-annot-20150608-git/misc")
+
+(require 'slime-annot)
+
+(slime)
--- org-babel-do-load-languages.el
+++ org-babel-do-load-languages.el
@@ -7,8 +7,11 @@
 (add-to-list 'package-archive-priorities '("org" . 5))
 (add-to-list 'package-archive-priorities '("melpa" . 4))
 (add-to-list 'package-archive-priorities '("built-in" . 3))
-          
 
+;; List of additional LaTeX packages
+(add-to-list 'org-export-latex-packages-alist '("" "cmap" t))
+(add-to-list 'org-export-latex-packages-alist '("english,russian" "babel" t))
+          
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((ditaa   . t)
