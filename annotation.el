c55956d32ce13e081fe5bbf4ef165f20440b422e
Nick Matvyeyev <mnasoft@gmail.com>
2020-08-31 16:48:24 +0300

    Mon Aug 31 16:48:24 EEST 2020

--- annotation.el
+++ annotation.el
@@ -1,5 +1,7 @@
 ;;;; /home/namatv/elisp/annotation.el
 
+
+
 (defun ann-enable ()
   "Предназначена для формирования вставки блока кода"
   (interactive)
@@ -79,3 +81,49 @@
   (interactive)
   (insert "@annot.class:export-accessors
 "))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun ann-rm-export ()
+  "Предназначена для формирования вставки блока кода"
+  (interactive)
+  (let ((s-pos 0) (e-pos))
+    (search-forward "@export")
+    (move-beginning-of-line 1)
+    (insert "(")
+    (delete-forward-char 1)
+    (move-end-of-line 1)
+    (insert " '")
+    (next-line 1)
+    (move-beginning-of-line 1)
+    (search-forward " ")
+    (setf s-pos (point))
+    (search-forward "(")
+    (left-char 1)
+    (setf e-pos (point))
+    (kill-ring-save s-pos e-pos)
+    (move-end-of-line 0)
+    (yank)
+    (insert ")")))
+
+(defun ann-rm-doc ()
+  "Предназначена для формирования вставки блока кода"
+  (interactive)
+  (let ((s-pos 0) (e-pos))
+    (search-forward "@doc")
+    (move-beginning-of-line 1)
+    (kill-line 1)
+    (setf s-pos (point))
+    (search-forward "(def")
+    (move-beginning-of-line 1)
+    (setf e-pos (point))
+    (kill-region s-pos e-pos)
+    (right-char 1)
+    (forward-list 1)
+    (newline 1)
+    (yank)
+    (delete-blank-lines)
+    (delete-blank-lines)
+    ))
+
+
