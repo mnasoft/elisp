e9af72733efe5babbab816ef36d8765bef28e619
Nick Matvyeyev <mnasoft@gmail.com>
2023-01-23 07:27:28 +0200

    23 янв 2023 г.  7:27:28

--- inferior-lisp/N142013.el
+++ inferior-lisp/N142013.el
@@ -1,4 +1,10 @@
 ;;;; ./inferior-lisp/N142013.el
 
-;;;;(setq inferior-lisp-program "d:/home/_namatv/PRG/SBCL/2.2.1-msi/sbcl.exe")
-(sly-connect "localhost" 4005)
+;;;;(progn (setq inferior-lisp-program "d:/home/_namatv/PRG/SBCL/2.2.1-msi/sbcl.exe") (sly))
+(progn
+  (setq sly-lisp-implementations
+        '((sbcl ("sbcl" "--dynamic-space-size" "16384"))))
+  (setq inferior-lisp-program
+        "d:/home/_namatv/PRG/SBCL/2.3.0-msi/sbcl.exe")
+  (sly))
+;;;; (sly-connect "localhost" 4005)
