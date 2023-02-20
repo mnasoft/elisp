7aed857d56e038fb00966f2641a09459c259501e
Mykola Matvyeyev <mnasoft@gmail.com>
2023-02-20 05:47:43 +0200

    Mon Feb 20 03:47:42 UTC 2023

--- inferior-lisp/uakazi-note.el
+++ inferior-lisp/uakazi-note.el
@@ -1,7 +1,7 @@
 
-(progn
-  (setq sly-lisp-implementations '((sbcl ("sbcl" "--dynamic-space-size" "4096"))))
-  (setq inferior-lisp-program "D:/PRG/SBCL/2.3.0-msi/sbcl.exe")
-  (sly))
-
-;;;;(sly-connect "localhost" 4005)
+(if t
+    (progn
+      (setq sly-lisp-implementations '((sbcl ("sbcl" "--dynamic-space-size" "4096"))))
+      (setq inferior-lisp-program "D:/PRG/SBCL/2.3.1-msi/sbcl.exe")
+      (sly))
+  (sly-connect "localhost" 4005))
