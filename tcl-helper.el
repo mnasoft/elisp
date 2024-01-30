3ed13130db12c26dd5596897211064956a81907d
Mykola Matvyeyev <mnasoft@gmail.com>
2024-01-30 22:40:35 +0200

    Tue Jan 30 20:40:34 UTC 2024

---
+++ tcl-helper.el
@@ -0,0 +1,121 @@
+;;;; ~/elisp/tcl-helper.el
+
+(defun find-proc-string-with-cursor ()
+  (interactive)
+  (let ((regexp "^\\s-*proc ")
+        (point-pos (point))
+        (end-pos (point-max)))
+    (when (re-search-forward
+           regexp end-pos t)
+      (concat (match-string 0)
+              (number-to-string
+               point-pos) "."))))
+
+(defun th-description-header ()
+  "Предназначена для формирования вставки блока кода"
+  (interactive)
+    (find-proc-string-with-cursor)
+  (left-char 5)
+  (setq p1 (point))
+  (search-forward "{")
+  (forward-sexp)
+  (setq p2 (point))
+  (kill-ring-save p1 p2)
+  (move-beginning-of-line 1)
+  (newline)
+  (previous-line)
+  (insert "## - =")
+  (yank 1)
+  (insert "= :: Возвращает ..."))
+
+
+(defun th-description ()
+  "Предназначена для формирования вставки блока кода"
+  (interactive)
+  (th-description-header)
+  (th-variables)
+  (th-example))
+ 
+(defun th-h1 ()
+ (interactive)
+ (move-end-of-line  1)
+ (newline)
+ (insert "## * "))
+
+(defun th-h2 ()
+ (interactive)
+ (move-end-of-line  1)
+ (newline)
+ (insert "## ** "))
+
+(defun th-h3 ()
+ (interactive)
+ (move-end-of-line  1)
+ (newline)
+ (insert "## *** "))
+
+(defun th-h4 ()
+ (interactive)
+ (move-end-of-line  1)
+ (newline)
+ (insert "## **** "))
+
+(defun th-h5 ()
+ (interactive)
+ (move-end-of-line  1)
+ (newline)
+ (insert "## ***** "))
+
+(defun th-h6 ()
+ (interactive)
+ (move-end-of-line  1)
+ (newline)
+ (insert "## ****** "))
+
+(defun th-indentation ()
+ (interactive)
+ (move-beginning-of-line 1)
+ (insert "## ")
+ (next-line))
+
+(defun th-unindentation ()
+ (interactive)
+ (move-beginning-of-line 1)
+ (insert "## ")
+ (next-line))
+
+(defun th-p ()
+  (interactive)
+  (th-line)
+  (th-line))
+
+(defun th-src ()
+  (interactive)
+  (th-line)
+  (insert "#+begin_src tcl")
+  (th-line)
+  (th-line)
+  (insert "#+end_src")
+  (previous-line))
+
+(defun th-example ()
+  (interactive)
+  (th-p)
+  (insert "Пример использования:")
+  (th-src))
+
+
+(defun th-variables ()
+  (interactive)
+  (th-p)
+  (insert "Аргументы:")
+  (th-line)
+  (insert "- ")
+  (th-line)
+  (insert "- "))
+
+(defun th-line ()
+ (interactive)
+ (move-end-of-line  1)
+ (newline)
+ (insert "##   "))
