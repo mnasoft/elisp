3be23293877e75b2ecda0f9bdc5edb878cefc7fd
Mykola Matvyeyev <mnasoft@gmail.com>
2026-06-15 07:17:48 +0300

    2026-06-15 07:17:48

--- kill-some-buffers.el
+++ kill-some-buffers.el
@@ -1,23 +1,22 @@
 ;;;; kill-some-buffers.el
 
-(defun kill-buffer-if-exist (b-name)
-  (let ((buff (get-buffer b-name)))
-    (when buff (kill-buffer buff))))
-
-(defun k-b ()
+(defun my/save-and-kill-all-file-buffers ()
   (interactive)
-  (kill-buffer-if-exist "*scratch*")
-  (kill-buffer-if-exist "*GNU Emacs*")
-  (kill-buffer-if-exist "*Completions*")
-  (kill-buffer-if-exist "*Messages*")
-  (kill-buffer-if-exist "*slime-events*")
-  (kill-buffer-if-exist "*slime-xref*")
-  (kill-buffer-if-exist "*Buffer List*")
-  (kill-buffer-if-exist "*Help*")
-  (kill-buffer-if-exist "*slime-compilation*")
-  (kill-buffer-if-exist "*shell*")
-  (kill-buffer-if-exist "*slime-description*")
-  (kill-buffer-if-exist "*slime-source*")
-  )
+  (save-some-buffers t)
+  (dolist (buf (buffer-list))
+    (with-current-buffer buf
+      (when buffer-file-name
+        (kill-buffer buf)))))
 
-;;;;(k-b)
+(defun my/kill-non-file-buffers-except-sly ()
+  (interactive)
+  (dolist (buf (buffer-list))
+    (with-current-buffer buf
+      (let ((name (buffer-name buf)))
+        (when (and (not buffer-file-name)
+                   (not (string-match-p "^ ?\\*sly" name)))
+          (kill-buffer buf))))))
+
+(global-set-key (kbd "C-c k f") #'my/save-and-kill-all-file-buffers)
+(global-set-key (kbd "C-c k n") #'my/kill-non-file-buffers-except-sly)
+
